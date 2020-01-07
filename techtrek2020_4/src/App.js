bad7cc55ccef3d73214b2f5b6b44fa59360d7469
ShaoKang <augustuscheddar@gmail.com>
2020-01-07 11:12:36 +0800

    Changed App.js

--- techtrek2020_4/src/App.js
+++ techtrek2020_4/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
+import Login from "./components/login/login"
+import Home from "./components/home/home"
+import ProtectedRoute from './protected.route/protected.route'
+import {BrowserRouter, Route, Switch} from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+    // var app = express()
+    // app.use(function(req, res, next) {
+    //   res.header("Access-Control-Allow-Origin", "*");
+    //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //   next();
+    // });
+    return (
+      <div align="center" className="backGround">
+        <nav>Some nav bar</nav>
+         <BrowserRouter>
+         
+         <Switch>
+        <Route exact path ="/" component = {Login}/>
+        <ProtectedRoute exact path ="/home" component = {Home}/>
+        <Route path ="*" component = {()=>"404 Not Found"}/>
+        </Switch>
+        </BrowserRouter>
+      </div>
+    )
+  
 }
 
 export default App;
